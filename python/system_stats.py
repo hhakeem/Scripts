83e472610bcc22a69697db8b0644f8332f734532
hhakeem <hhakeem@hhden.com>
2019-11-12 22:44:57 -0500

    Adding user_info, system_misc, and storage_info

--- python/system_stats.py
+++ python/system_stats.py
@@ -3,7 +3,7 @@
 import os
 import subprocess
 
-def host_info()
+def host_info():
     get_hostname = subprocess.Popen("hostname", shell=True, stdout=subprocess.PIPE)
     hostname = subprocess.getoutput("hostname")
     date = subprocess.getoutput("date +%Y%m%d-%H%M%S")
@@ -21,7 +21,7 @@ def host_info()
     print ("Uptime: ",uptime )
     print (" ")
 
-def network_info()
+def network_info():
 
     #IFCONFIG
     get_ip_info = subprocess.getoutput("ip a")
@@ -39,7 +39,7 @@ def network_info()
     print ("----- Host Route Info -----\n",get_route_info)
     print (" ")
 
-def storage_info()
+def storage_info():
     get_vol_info = subprocess.getoutput("df -h")
     get_lvm_pv_info = subprocess.getoutput("pvs")
     get_lvm_vg_info = subprocess.getoutput("vgs")
@@ -49,7 +49,7 @@ def storage_info()
     #Add autofs info
     get_nfs_exports_info = subprocess.getoutput("/bin/cat /etc/exports")
     get_smb_conf_info = subprocess.getoutput("/bin/cat /etc/samba/smb.conf")
-    get_isci_init_info = subprocess.getoutput("/bin/cat /etc/iscsi/initiatorname.iscsi")
+    get_iscsi_init_info = subprocess.getoutput("/bin/cat /etc/iscsi/initiatorname.iscsi")
     get_iscsi_dscv_info = subprocess.getoutput("/sbin/iscsiadm -m discovery -o show")
     get_iscsi_iface_info = subprocess.getoutput("/sbin/isciadmin -m iface")
     get_iscsi_iface2_info = subprocess.getoutput("/sbin/isciadmin -m iface -P 1")
@@ -61,28 +61,78 @@ def storage_info()
     print ("======================== Storage Information ========================")
     print (" ")
     print ("----- Volume Info -----\n",get_vol_info)
-#DF
+    print (" ")
+    print ("----- PV Info -----\n",get_lvm_pv_info)
+    print (" ")
+    print ("----- VG Info -----\n",get_lvm_vg_info)
+    print (" ")
+    print ("----- LV Info -----\n",get_lvm_lv_info)
+    print (" ")
+    print ("----- FDISK Info -----\n",get_fdisk_info)
+    print (" ")
+    print ("----- FSTAB Info -----\n",get_fstab_info)
+    print (" ")
+    print ("----- NFS Exports Info -----\n",get_nfs_exports_info)
+    print (" ")
+    print ("----- SMB Conf Info -----\n",get_smb_conf_info)
+    print (" ")
+    print ("----- ISCSI Init Info -----\n",get_iscsi_init_info)
+    print (" ")
+    print ("----- ISCSI DISCOVERY Info ------\n",get_iscsi_dscv_info)
+    print (" ")
+    print ("----- ISCSI IFACE INFO -----\n",get_iscsi_iface_info)
+    print (" ")
+    print (get_iscsi_iface_info)
+    print (" ")
+    print ("----- ISCSI SESSION Show Info -----\n",get_iscsi_session_show_info)
+    print (" ")
+    print ("----- ISCSI Node Info -----\n",get_iscsi_node_info)
+    print (" ")
+    print ("----- Multipath Info -----\n",get_multipath_info)
+    print (" ")
 
-#PVS
-#VGS
-#LVS
-#FDISK 
-#FSTAB
-#NFS
 
-def user_info() 
 
-#Users
-#Groups
-#Sudoers
+def user_info(): 
+    get_users = subprocess.getoutput("/bin/cat /etc/passwd")
+    get_groups = subprocess.getoutput("/bin/cat /etc/group")
+    get_sudoers = subprocess.getoutput("/bin/cat /etc/sudoer | grep -v \#")
+    get_sudoers.d = subprocess.getoutput("/bin")
+    
+    print ("======================== User Info ========================")
+    print (" ")
+    print ("----- Users Info -----\n",get_users)
+    print (" ")
+    print ("----- Groups Info -----\n",get_groups)
+    print (" ")
 
+
+
+def system_misc():
+    #get_active_ps = subprocess.getoutput("/bin/ps -ef  | grep -v root | grep -v xfs | grep -v sshd | grep -v bash | grep -v gdm | grep -v rpc | grep -v dbus | grep -v hald | grep -v ps | grep -v tee | grep -v mutt")
+    get_active_ps = subprocess.getoutput("/bin/ps -ef | grep -vE 'ps|dbus|xfs|root|sshd|gdm|rpc|dns|chronyd|lsmd|polkitd|rtkit|avahi|qmgr|colord|pickup'")
+    get_chkcfg_list = subprocess.getoutput("/sbin/chkconfig --list")
+    get_systemctl = subprocess.getoutput("/usr/bin/systemctl")
+    get_sysctl_conf = subprocess.getoutput("/bin/cat /etc/sysctl.conf")
+    get_security_lmts = subprocess.getoutput("/bin/cat /etc/security/limits.conf | grep -v \#")
+
+    print ("======================== System Misc. ========================")
+    print (" ")
+    print ("----- Active Processes -----\n",get_active_ps)
+    print (" ")
+    print ("----- Check Config List -----\n",get_chkcfg_list)
+    print (" ")
+    print ("----- Systemctl Info -----\n",get_systemctl)
+    print (" ")
+    print ("----- Security Limits Info -----\n",get_security_lmts)
+    print (" ")
 
 
 host_info()
 network_info()
 storage_info()
 user_info()
-
+system_misc()
 
 
 #take all function output to a file, and to email. 
